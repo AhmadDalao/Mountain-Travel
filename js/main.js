17dba647c43d96f0831933fbae031d0132acba97
shadow8evil@gmail.com <shadow8evil@gmail.com>
2020-05-02 18:23:59 +0300

    about us section is done and responsive

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 // navbar responsive function
 function myFunction() {
     var x = document.getElementById("navbar-not-responsive");
+
     if (x.className === "navbar-list") {
         x.className += " responsive";
         document.getElementById("my-iconX").style.display = "block";
         document.getElementById("my-icon").style.display = "none";
+        document.getElementById("navbar").style.backgroundColor = "#444444";
 
     } else {
         x.className = "navbar-list";
         document.getElementById("my-iconX").style.display = "none";
         document.getElementById("my-icon").style.display = "block";
+        document.getElementById("navbar").style.backgroundColor = "unset";
     }
 }
