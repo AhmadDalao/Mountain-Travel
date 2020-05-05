3586e8777b599ac9de3de6121d2369678f9a409a
shadow8evil@gmail.com <shadow8evil@gmail.com>
2020-05-05 22:08:06 +0300

    adding classes to the navbar on scroll

--- js/main.js
+++ js/main.js
@@ -104,3 +104,15 @@
               document.getElementById("my-icon").style.display = "block";
           }
       }
+
+      var myNav = document.getElementById('navbar');
+      window.onscroll = function() {
+          "use strict";
+          if (document.body.scrollTop >= 550 || document.documentElement.scrollTop >= 550) {
+              myNav.classList.add("nav-colored");
+              myNav.classList.remove("nav-transparent");
+          } else {
+              myNav.classList.add("nav-transparent");
+              myNav.classList.remove("nav-colored");
+          }
+      };
