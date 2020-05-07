cb79fbb0bf82a11d7c9130066f805ce4b3824aa5
shadow8evil@gmail.com <shadow8evil@gmail.com>
2020-05-07 23:21:57 +0300

    trying smooth scroll js code

--- js/main.js
+++ js/main.js
@@ -116,3 +116,13 @@
               myNav.classList.remove("nav-colored");
           }
       };
+
+      function scrollTo() {
+          var links = document.getElementsByTagName('a');
+          for (var i = 0; i < links.length; i++) {
+              var link = links[i];
+              if ((link.href && link.href.indexOf('#') != -1) && ((link.pathname == location.pathname) || ('/' + link.pathname == location.pathname)) && (link.search == location.search)) {
+                  link.onclick = scrollAnchors;
+              }
+          }
+      }
